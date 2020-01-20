8aedf77be69e916f9e7c75ff92c1c050561cc7f2
carmelobalzano <balzanoitaliano@gmail.com>
2020-01-19 19:09:42 -0500

    connected recent posts to redux

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -8,7 +8,7 @@ class SearchBar extends Component {
     handleFormSubmit = function({query}) {
         console.log('trying to handle submit for query', query);
         //navigate to a new route.
-        this.props.history.push('/results');
+        this.props.history.push('./results');
     }
 
     renderInput(field) {
