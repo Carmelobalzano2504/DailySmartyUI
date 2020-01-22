991c4d7f60b6ab8a2d1fbbca0554a693ebb3d450
carmelobalzano <balzanoitaliano@gmail.com>
2020-01-22 17:12:26 -0500

    fetchPostWithQuery

--- src/components/results.js
+++ src/components/results.js
@@ -3,15 +3,23 @@ import React, { Component } from 'react';
 import Logo from './logo';
 import SeachBar from './searchBar';
 
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 class Results extends Component {
+
+    handleSearchBarSubmit(query) {
+        this.props.fetchPostsWithQuery(query);
+    }
+
     render() {
         return (
             <div>
               <Logo size={55} />
-              <SeachBar/>
+              <SeachBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
             </div>
         )
     }
 }
 
-export default Results;
+export default connect(null, actions)(Results);
